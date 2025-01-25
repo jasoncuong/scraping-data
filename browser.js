6d621b52a7bcc4d2ab546790781bdc501f39da3c
masonnguyen <100501225+jasoncuong@users.noreply.github.com>
2025-01-25 14:58:58 +0700

    Init

--- browser.js
+++ browser.js
@@ -1,19 +1,17 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require("puppeteer");
 
 const startBrowser = async () => {
-    let browser
-    try {
-        browser = await puppeteer.launch({
-            headless: true,
-            args: ["--disable-setuid-sandbox"],
-            'ignoreHTTPSErrors': true
-        })
+  let browser;
+  try {
+    browser = await puppeteer.launch({
+      headless: true,
+      args: ["--disable-setuid-sandbox"],
+      ignoreHTTPSErrors: true,
+    });
+  } catch (error) {
+    console.log("Không tạo được browser: " + error);
+  }
+  return browser;
+};
 
-    } catch (error) {
-        console.log('Không tạo được browser: ' + error)
-    }
-    return browser
-
-}
-
-module.exports = startBrowser
+module.exports = startBrowser;
